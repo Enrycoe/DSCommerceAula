17f78b545e26ff0d807428388932457ca74747ed
Henrique Rafael Ardina <henrique.ardina@gmail.com>
2023-05-16 19:55:49 -0300

    OrderItem, OrderItemPK

--- src/main/java/com/henriqueardina/dscommerce/entities/Product.java
+++ src/main/java/com/henriqueardina/dscommerce/entities/Product.java
@@ -3,8 +3,6 @@ package com.henriqueardina.dscommerce.entities;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.springframework.beans.factory.annotation.Value;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -32,6 +31,9 @@ public class Product {
 	@ManyToMany
 	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
+	
+	@OneToMany(mappedBy ="id.product" )
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Product() {
 	}

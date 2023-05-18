27739681e455d46d4ff44819887a9a73fb35485b
Henrique Rafael Ardina <henrique.ardina@gmail.com>
2023-05-17 21:09:26 -0300

    GET product by id

---
+++ src/main/java/com/henriqueardina/dscommerce/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.henriqueardina.dscommerce.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.henriqueardina.dscommerce.dto.ProductDTO;
+import com.henriqueardina.dscommerce.entities.Product;
+import com.henriqueardina.dscommerce.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	@Transactional(readOnly = true)
+	public ProductDTO findById(Long id) {
+		Product product = repository.findById(id).get();
+		return new ProductDTO(product);
+		
+		
+	}
+}
